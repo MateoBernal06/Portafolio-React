3059679446a9d15aba66b19017930a7f6d39d553
MateoBernal06 <133398097+MateoBernal06@users.noreply.github.com>
2026-04-21 21:39:29 -0500

    arreglo nombre archivos

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import 'rsuite/styles/index.less';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { Head } from "./components/Head.jsx"
-import { About } from "./components/About.jsx"
-import { Projects } from "./components/Projects.jsx"
-import { Certificate } from "./components/Certificate.jsx"
-import { Footer } from "./components/Footer.jsx"
-import { Contact } from "./components/Contact.jsx"
-import { Experience } from "./components/Experience.jsx"
+import { Head } from "./components/head.jsx"
+import { About } from "./components/about.jsx"
+import { Projects } from "./components/projects.jsx"
+import { Certificate } from "./components/certificate.jsx"
+import { Footer } from "./components/footer.jsx"
+import { Contact } from "./components/contact.jsx"
+import { Experience } from "./components/experience.jsx"
 import "./components/components.css"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
