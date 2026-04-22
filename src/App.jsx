2d8a57009edfabe4f7fb1cff71adb4068aee0164
MateoBernal06 <133398097+MateoBernal06@users.noreply.github.com>
2026-04-21 20:26:46 -0500

    Despliegue

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import 'rsuite/styles/index.less';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { Head } from "./components/Head.jsx"
-import { About } from "./components/About.jsx";
-import { Projects } from "./components/Projects.jsx";
-import { Certificate } from "./components/Certificate.jsx";
-import { Footer } from "./components/Footer.jsx";
-import { Contact } from "./components/Contact.jsx";
-import { Experience } from "./components/Experience.jsx";
-import "./components/components.css";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { About } from "./components/About.jsx"
+import { Projects } from "./components/Projects.jsx"
+import { Certificate } from "./components/Certificate.jsx"
+import { Footer } from "./components/Footer.jsx"
+import { Contact } from "./components/Contact.jsx"
+import { Experience } from "./components/Experience.jsx"
+import "./components/components.css"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
 const App = () => {
   return (
